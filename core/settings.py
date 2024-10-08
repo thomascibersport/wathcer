777eb391e58a7520f808e6c2b810406e2e128826
root <thomascibersport@gmail.com>
2024-10-08 23:32:35 +0300

    1

--- core/settings.py
+++ core/settings.py
@@ -73,5 +73,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- main/models.py
+++ main/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class PreviewImage(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
+class Contact(models.Model):
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.phone} - {self.email}'
